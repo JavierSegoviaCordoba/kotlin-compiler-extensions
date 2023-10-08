d702682eaae1ae18b7841abd01ad24580028194f
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-10-08 21:16:19 +0200

    Change semver Kotlin version mapping

--- kotlin-compiler-extensions/build.gradle.kts
+++ kotlin-compiler-extensions/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.android.build.gradle.internal.tasks.factory.dependsOn
+import com.javiersc.gradle.version.GradleVersion
+import com.javiersc.gradle.version.isSnapshot
 import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 
 hubdle {
@@ -17,8 +19,7 @@ hubdle {
         versioning {
             semver {
                 mapVersion { gradleVersion ->
-                    val mappedVersion = gradleVersion.copy(metadata = getKotlinPluginVersion())
-                    "$mappedVersion"
+                    gradleVersion.mapIfKotlinVersionIsProvided(getKotlinPluginVersion())
                 }
             }
         }
@@ -81,3 +82,31 @@ tasks.named("test").configure {
 tasks.named("allTestsReport").configure { dependsOn(checkCompilerExtensionsAreCalled) }
 
 tasks.check.dependsOn(checkCompilerExtensionsAreCalled)
+
+fun GradleVersion.mapIfKotlinVersionIsProvided(kotlinVersion: String): String {
+    val major: Int = major
+    val minor: Int = minor
+    val patch: Int = patch
+    check(kotlinVersion.isKotlinDevVersion()) {
+        """ |Kotlin version: $kotlinVersion
+            |Requirements to use a specific Kotlin version:  
+            | - It must be a dev version, for example: `1.9.20-dev-5788`
+            |Check the Kotlin dev versions on the bootstrap repository:
+            |  - https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/org/jetbrains/kotlin/ 
+        """
+            .trimMargin()
+    }
+    check(isSnapshot) {
+        """ |Current version: ${this@mapIfKotlinVersionIsProvided}
+            |Kotlin version: $kotlinVersion
+            |Requirements to use a specific Kotlin version:  
+            | - Use a SNAPSHOT version with `-P semver.stage=snapshot`
+            | - Clean repo or use `-P semver.checkClean=false`
+        """
+            .trimMargin()
+    }
+    return "$major.$minor.$patch+$kotlinVersion-SNAPSHOT"
+}
+
+fun String.isKotlinDevVersion(): Boolean =
+    matches(Regex("""(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)-dev-(0|[1-9]\d*)"""))
--- kotlin-compiler-test-extensions/build.gradle.kts
+++ kotlin-compiler-test-extensions/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.javiersc.gradle.version.GradleVersion
+import com.javiersc.gradle.version.isSnapshot
 import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 
 hubdle {
@@ -15,8 +17,7 @@ hubdle {
         versioning {
             semver {
                 mapVersion { gradleVersion ->
-                    val mappedVersion = gradleVersion.copy(metadata = getKotlinPluginVersion())
-                    "$mappedVersion"
+                    gradleVersion.mapIfKotlinVersionIsProvided(getKotlinPluginVersion())
                 }
             }
         }
@@ -37,3 +38,31 @@ hubdle {
         }
     }
 }
+
+fun GradleVersion.mapIfKotlinVersionIsProvided(kotlinVersion: String): String {
+    val major: Int = major
+    val minor: Int = minor
+    val patch: Int = patch
+    check(kotlinVersion.isKotlinDevVersion()) {
+        """ |Kotlin version: $kotlinVersion
+            |Requirements to use a specific Kotlin version:  
+            | - It must be a dev version, for example: `1.9.20-dev-5788`
+            |Check the Kotlin dev versions on the bootstrap repository:
+            |  - https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/org/jetbrains/kotlin/ 
+        """
+            .trimMargin()
+    }
+    check(isSnapshot) {
+        """ |Current version: ${this@mapIfKotlinVersionIsProvided}
+            |Kotlin version: $kotlinVersion
+            |Requirements to use a specific Kotlin version:  
+            | - Use a SNAPSHOT version with `-P semver.stage=snapshot`
+            | - Clean repo or use `-P semver.checkClean=false`
+        """
+            .trimMargin()
+    }
+    return "$major.$minor.$patch+$kotlinVersion-SNAPSHOT"
+}
+
+fun String.isKotlinDevVersion(): Boolean =
+    matches(Regex("""(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)-dev-(0|[1-9]\d*)"""))
