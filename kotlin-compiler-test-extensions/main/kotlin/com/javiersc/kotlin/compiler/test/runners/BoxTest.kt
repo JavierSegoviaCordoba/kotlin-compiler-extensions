7e6b868b8b565eed9ee792a66c2b9374cd3ad7d1
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-11-01 20:01:09 +0100

    Enable context receivers by default in tests

--- kotlin-compiler-test-extensions/main/kotlin/com/javiersc/kotlin/compiler/test/runners/BoxTest.kt
+++ kotlin-compiler-test-extensions/main/kotlin/com/javiersc/kotlin/compiler/test/runners/BoxTest.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.test.builders.fir2IrStep
 import org.jetbrains.kotlin.test.builders.irHandlersStep
 import org.jetbrains.kotlin.test.builders.jvmArtifactsHandlersStep
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.DUMP_IR
+import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
 import org.jetbrains.kotlin.test.model.FrontendKind
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.runners.RunnerWithTargetBackendForTestGeneratorMarker
@@ -34,7 +35,10 @@ public abstract class BoxTest : BaseTestRunner(), RunnerWithTargetBackendForTest
     public open fun commonServicesConfiguration(): ((FrontendKind<*>) -> Unit)? = null
 
     override fun TestConfigurationBuilder.configuration() {
-        defaultDirectives { +DUMP_IR }
+        defaultDirectives {
+            +DUMP_IR
+            LanguageSettingsDirectives.LANGUAGE with "+ContextReceivers"
+        }
 
         commonPluginConfiguration(
             classpathProvider = runtimeClasspathProvider,
