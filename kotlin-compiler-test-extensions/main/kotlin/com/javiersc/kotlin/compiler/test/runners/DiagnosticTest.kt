91131cfba20e6bf1f23c12de0c0cfda0e826fad8
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2024-12-13 01:27:02 +0100

    Change Kotlin version to `2.1.20-RC2`

--- kotlin-compiler-test-extensions/main/kotlin/com/javiersc/kotlin/compiler/test/runners/DiagnosticTest.kt
+++ kotlin-compiler-test-extensions/main/kotlin/com/javiersc/kotlin/compiler/test/runners/DiagnosticTest.kt
@@ -1,11 +1,11 @@
 package com.javiersc.kotlin.compiler.test.runners
 
+import com.javiersc.kotlin.compiler.test.services.AdditionalFilesProvider
 import com.javiersc.kotlin.compiler.test.services.MetaRuntimeClasspathProvider
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar.ExtensionStorage
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.test.Constructor
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
-import org.jetbrains.kotlin.test.model.FrontendKind
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
 import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
@@ -14,24 +14,27 @@ public abstract class DiagnosticTest : BaseTestRunner() {
 
     public open val runtimeClasspathProvider: Constructor<MetaRuntimeClasspathProvider>? = null
 
+    public open val additionalFilesProvider: Constructor<AdditionalFilesProvider>? = null
+
     public abstract fun ExtensionStorage.registerExtensions(
         module: TestModule,
         configuration: CompilerConfiguration,
     )
 
-    public open fun commonServicesConfiguration(): ((FrontendKind<*>) -> Unit)? = null
-
-    override fun TestConfigurationBuilder.configuration() {
-        commonPluginConfiguration(
-            classpathProvider = runtimeClasspathProvider,
-            registerCompilerExtensions = { module, configuration ->
-                registerExtensions(module, configuration)
-            },
-            commonServicesConfiguration = commonServicesConfiguration(),
-        )
-    }
+    override fun configure(builder: TestConfigurationBuilder): Unit =
+        with(builder) { configuration() }
 
     override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
         return EnvironmentBasedStandardLibrariesPathProvider
+    }
+
+    private fun TestConfigurationBuilder.configuration() {
+        commonPluginConfiguration(
+            classpathProvider = runtimeClasspathProvider,
+            additionalFilesProvider = additionalFilesProvider,
+            registerCompilerExtensions = { module, configuration ->
+                registerExtensions(module, configuration)
+            },
+        )
     }
 }
